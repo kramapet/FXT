00e6f8f17accdae54781a4c81903b5881649e86f
Tom Marek <angelo.sharpe@gmail.com>
2014-04-07 15:20:24 +0200

    Implemented indexing of symbols
    TODO: append, store and load to/from pickle

--- FXT_datastore/src/symbol.py
+++ FXT_datastore/src/symbol.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+import bisect
 
 class Symbol(list):
     def __init__(self, symbol_name, values, dates):
@@ -11,37 +12,52 @@ class Symbol(list):
         
         self.data_updated = False
         
-    def _dt_to_idx(self, date, larger=True):
-        # TODO, find closest date from dates and return index
-        # if larger is set to true searching for closest larger date, else for smaller
-        print(min(self, key=lambda x: abs(date-x)))
-        
-    def __getitem__(self, date):
-        if isinstance(date, slice):
-            start = date.start
-            end = date.stop
-            if ((isinstance(start, int) or (start == None)) and (isinstance(end, int) or (end == None))):
-                return Symbol(self.symbol_name, list.__getitem__(self, date), list.__getitem__(self.dates, date))
-            elif ((isinstance(start, datetime.date) or (start == None)) and (isinstance(end, datetime.date) or (end == None))):
-                #return [list.__getitem__(self, i) for i in range(start, end)]
-                pass
+    def _dt_to_idx(self, date, left=None):
+        if date is None:
+            return None
+        if left is None:
+            i = bisect.bisect_left(self.dates, date)
+            if i != len(self.dates) and self.dates[i] == date:
+                return i
+            raise(ValueError)
+        elif left == True:
+            # Find leftmost item greater than or equal to date
+            i = bisect.bisect_left(self.dates, date)
+            if i != len(self.dates):
+                return i
+            raise ValueError
+        else:
+            # Find rightmost value less than x
+            i = bisect.bisect_left(self.dates, date)
+            if i:
+                return i
+            raise ValueError
+
+    def __getitem__(self, arg):
+        if isinstance(arg, slice):
+            if ((isinstance(arg.start, int) or (arg.start == None)) and (isinstance(arg.stop, int) or (arg.stop == None))):
+                return Symbol(self.symbol_name, list.__getitem__(self, arg), list.__getitem__(self.dates, arg))
+            elif ((isinstance(arg.start, datetime.datetime) or (arg.start == None)) and (isinstance(arg.stop, datetime.datetime) or (arg.stop == None))):
+                start = self._dt_to_idx(arg.start, left=True)
+                stop = self._dt_to_idx(arg.stop, left=False)
+                s = slice(start, stop)
+                return Symbol(self.symbol_name, list.__getitem__(self, s), list.__getitem__(self.dates, s))
             else:
                 raise(TypeError, "Invalid argument type.")  
-                
-        elif isinstance(date, datetime.date):
-            index = self.get_index(date)
+        elif isinstance(arg, datetime.datetime):
+            index = self._dt_to_idx(arg)
             return list.__getitem__(self, index)
-        elif isinstance(date, int):
-            if date < 0:
-                date += len(self)
-            if date >= len(self):
-                raise(IndexError, "index out of range {}".format(date))
-            return list.__getitem__(self, date)
+        elif isinstance(arg, int):
+            if arg < 0:
+                arg += len(self)
+            if arg >= len(self):
+                raise(IndexError, "index out of range {}".format(arg))
+            return (self.dates[arg], list.__getitem__(self, arg))
         else:
             raise(TypeError, "Invalid argument type.")  
     
     def __str__(self):
-        output = "Symbol class name: " + self.symbol_name + "\n"
+        output = "Symbol name: " + self.symbol_name + "\n"
         size = list.__len__(self)
         if size < 20:
             for i in range(size):
@@ -52,11 +68,7 @@ class Symbol(list):
             output += "    ...\n"
             for i in range(5, 0, -1):
                 output += "  " + str(self.dates[i]) + ": " + str(list.__getitem__(self, i)) + "\n"
-        
         return output
-        
-    def __repr__(self):
-        pass
 
     def append(self, data, dates=None):
         """Append data to the data and dates fields"""
@@ -70,7 +82,6 @@ class Symbol(list):
                     # format: data = [((ask, bid), date), ... ]
                     list.append(entry[1])
                     self.dates.append(entry[0])
-                    
         else:
             # format: data = [(ask, bid), ...] , dates = (date, ...)
             list.append(data)
@@ -80,15 +91,25 @@ class Symbol(list):
         pass
     
     def save(self, path):
-        pass
+        if self.data_updated:
+            # save to disk
+            pass
     
     def load(self, path):
         pass
 
 
 if __name__ == '__main__':
-    symbol = Symbol("EURUSD", [(0,1), (1,2), (2,3), (3,4), (4,5)], [datetime.date(2000, 1, (1+i)*2) for i in range (5)])
-    print(symbol[1:2])
+    symbol = Symbol("EURUSD", [(0,1), (1,2), (2,3), (3,4), (4,5), (5,6), (6,7), (7,8), (8,9), (9,10)], [datetime.datetime(2000, 1, (1+i)*2) for i in range (10)])
+    print(symbol[:])
+    #print(symbol[1:3])
+    #print(symbol[1:-1])
+    #print(symbol[1])
+    #print(symbol[datetime.datetime(2000, 1, 4)])
+    #print(symbol[datetime.datetime(2000, 1, 4, hour=20, minute=30, second=0):])
+    #print(symbol[:datetime.datetime(2000, 1, 4, hour=20, minute=30, second=0)])
+    print(symbol[datetime.datetime(2000, 1, 5, hour=23, minute=59, second=59):datetime.datetime(2000, 1, 10, hour=0, minute=0, second=1)])
+    
     #symbol._dt_to_idx(datetime.date(2000, 1, 3, 12))
         
 
