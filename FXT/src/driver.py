f0a0f992136c4afeaac9ad4036a3c4333213effa
Tom Marek <angelo.sharpe@gmail.com>
2014-06-21 23:05:37 +0200

    Add support for config / dynamical module loading
    the config file is in the json format

--- FXT/src/driver.py
+++ FXT/src/driver.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 from datetime import datetime
 
-from src.mock_model import MockModel
 from src.mock_broker import MockBroker
 from src.test_broker import TestBrokerLocal
 
@@ -12,15 +12,27 @@ class Driver():
     Driver takes care of selecting model, broker and regime
     """
     def __init__(self):
-        self.broker = self.init_broker()
-        self.model = self.init_model()
+        self.config = self.read_config()
 
-    def init_broker(self):
-        return TestBrokerLocal(account_balance=1000, margin_rate=0.05, start_date=datetime(2014, 4, 1), end_date=datetime.now())
+        self.broker = self.init_module('test_broker')
+        self.model = self.init_module('model')
 
-    def init_model(self):
-        return MockModel(('EUR','USD'))
+    def read_config(self):
+        with open('config.json', 'r') as config:
+            config = json.load(config)
+        return config
+
+    def init_module(self, module):
+        import_string = "from " + self.config[module]['import'] + \
+                        " import " + self.config[module]['class']
+        params = ""
+        for param_name in self.config[module]['params']:
+            params += param_name + "=" + str(self.config[module]['params'][param_name]) + ", "
+
+        exec(import_string)
+        return eval(self.config[module]['class'] + "(" + params[:-2] + ")")
 
     def start(self):
         self.model.trade(self.broker)
+
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
