960b282a1364c363ef7f4fa0cf349c5527774ef2
Tom Marek <angelo.sharpe@gmail.com>
2014-06-26 15:18:07 +0200

    Test broker updated to behave more like real one

--- FXT/src/brokers/test_broker.py
+++ FXT/src/brokers/test_broker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
 from datetime import datetime
 
 from src.local_data import LocalData
@@ -9,45 +10,100 @@ from src.trade import Trade
 
 
 class TestBrokerLocal():
-    def __init__(self, account_balance, margin_rate, start_date, end_date):
-        self.wallet = account_balance
-        self.test_data = {'start_date':start_date, 'end_date':end_date}
+    def __init__(self, account_balance, margin_rate, start_date, end_date, account_currency="EUR", log_level='WARNING'):
+        self.account_id = None
+        self.account_name = 'Local test'
+        self.account_currency = account_currency
         self.margin_rate = margin_rate
 
-        self.stat = Stat(account_balance)
+        self.balance = account_balance
+        self.margin_available = account_balance
+        self.margin_used = 0.0
+
+        self.open_orders = 0
+        self.open_orders_list = []
+        self.open_trades = 0
+        self.open_trades_list = []
+
+        self.realized_pl = 0
+        self.unrealized_pl = 0
+
+        self.test_data = {'start_date':start_date, 'end_date':end_date}
         self.local_data = LocalData()
 
+        self.stat = Stat(account_balance)
         self.last_tick = {}
+
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel('DEBUG')
+
+    def __str__(self):
+        ret = "Local test broker"
+        ret += "account id: " + str(self.account_id) + "\n"
+        ret += "account name: " + str(self.account_name) + "\n"
+        ret += "account currency: " + str(self.account_currency) + "\n"
+        ret += "balance: " + str(self.balance) + "\n"
+        ret += "margin available: " + str(self.margin_available) + "\n"
+        ret += "margin used: " + str(self.margin_used) + "\n"
+        ret += "margin rate:  " + str(self.margin_rate) + "\n"
+        ret += "realized pl: " + str(self.realized_pl) + "\n"
+        ret += "unrealized pl:  " + str(self.unrealized_pl) + "\n"
+        return ret
 
     def get_tick_data(self, instrument):
         for tick in self.local_data.read_tfx_files(instrument, self.test_data['start_date'], self.test_data['end_date']):
             self.stat.add_tick(tick)
             self.last_tick[instrument] = tick
+
+            # close finished sl/tp trades
+            self.open_trades_list = self.close_finished_trades(self.open_trades_list)
+
             yield tick
 
-    def open(self, instrument, volume):
-        if volume > 0:
-            price = self.last_tick[instrument].buy
-        elif volume < 0:
-            price = self.last_tick[instrument].sell
+    def open(self, instrument, volume, order_type='market', expiry=None, **args):  # price=None, lower_bound=None, upper_bound=None, sl=None, tp=None, ts=None):
+        if order_type == 'market':
+            if volume > 0:
+                open_price = self.last_tick[instrument].buy
+            elif volume < 0:
+                open_price = self.last_tick[instrument].sell
 
-        trade = Trade(instrument, volume, price, self.margin_rate)
-        self.wallet -= trade.margin
-
-        return trade
+            trade = Trade(instrument=instrument,
+                          volume=volume,
+                          open_price=open_price,
+                          open_datetime=self.last_tick[instrument].datetime)
+            trade_margin = (abs(volume) * open_price) * self.margin_rate
+            if self.margin_available - trade_margin < 0:
+                print("Not enough money to open the trade")
+                return None
+            else:
+                self.margin_available -= trade_margin
+                self.margin_used += trade_margin
+                self.open_trades += 1
+                self.open_trades_list.append(trade)
+                return trade
+        else:
+            self.logger.warning("oreder_type is not 'market' - not implemented yet")
 
     def close(self, trade):
         if trade.volume > 0:
-            price = self.last_tick[trade.instrument].sell
+            close_price = self.last_tick[trade.instrument].sell
         elif trade.volume < 0:
-            price = self.last_tick[trade.instrument].buy
+            close_price = self.last_tick[trade.instrument].buy
 
-        trade.close(price)
+        trade.close(close_price, self.last_tick[trade.instrument].datetime)
+        trade_margin = (abs(trade.volume) * close_price) * self.margin_rate
 
-        self.wallet += trade.margin
-        self.wallet += trade.get_profit()
+        self.margin_available += trade_margin
+        self.margin_used -= trade_margin
+
+        # get profit
+        pl = (trade.close_rate - trade.open_rate) * abs(trade.volume)
+        self.realized_pl += pl
+        self.balance += pl
+        trade.set_profit(pl)
 
         self.stat.add_trade(trade)
+        return trade
 
     def convert_currency(self, instrument, base_volume, rate=None):
         if rate:
@@ -96,21 +152,17 @@ class TestBrokerLocal():
                         left_trades.append(trade)
         return left_trades
 
-    def get_pips(self, instrument, rate1, rate2):
-        if instrument[0] == 'JPY' or instrument[1] == 'JPY':
-            pip = 0.01
-        else:
-            pip = 0.0001
-        return int((rate2 - rate1) / pip)
+    def get_account(self):
+        """Does nothing in local implementation
+        """
+        None
 
-    def get_account_info(self):
-        self.margin_rate = 0.05
-        self.account_currency = 'EUR'
+    def get_account_information(self):
+        """Does nothing in local implementation
+        """
+        None
 
-    def get_account_state(self):
-        self.wallet = 1000
-
-    def get_active_trades(self):
+    def get_open_trades(self):
         return []
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
