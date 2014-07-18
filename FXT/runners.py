54dafc02ccbccaef1c59c39c1785ebed0d3a0bed
Petr Kramar <kramar@masquerade.cz>
2014-07-18 21:07:35 +1200

    FXT.runners.CliRunner + tests

--- FXT/runners.py
+++ FXT/runners.py
@@ -1,11 +1,80 @@
-from abc import ABCMeta
+from argparse import ArgumentParser
 
-class AbstractRunner(metaclass=ABCMeta):
-	pass
+class BaseRunner:
 
-class CliRunner(AbstractRunner):
 	def run(self):
-		print("CliRunner")
+		while self.broker.has_tick():
+			self.model.beforeTick()
+			self.model.processTick(self.broker.get_tick())
+			self.model.afterTick()
+			self.broker.afterTick()
+
+		self.renderer.process(self.broker.get_transactions(), self.broker.get_info(), self.model.get_info())
+
+
+
+class CliRunner(BaseRunner):
+	
+	def __init__(self, loader):
+		self.loader = loader
+
+	def parse_arguments(self, args):
+		entities = ('broker', 'model')
+
+		broker_class = self.loader.load(args[0])
+		model_class = self.loader.load(args[1])
+		parser = ArgumentParser()
+		parser.add_argument('broker', help='Broker class')
+		parser.add_argument('model', help='Model class')
+
+		for entity in entities:
+			self.__add_entity_arguments(parser, entity, broker_class)
+			self.__add_entity_arguments(parser, entity, model_class)
+
+
+		return parser.parse_args(args)
+
+	def __add_entity_arguments(self, parser, entity, entity_class):
+		"""Add entity arguments
+
+		Check if constructor of entity class has annotated
+		arguments to use proper type, non-annotated arguments
+		will be filled as strings
+
+		Keyword arguments:
+		parser -- <ArgumentParser>
+		entity -- <string> {model,broker,renderer}
+		entity_class -- <class>
+		"""
+		
+		# constructor is not defined
+		if not hasattr(entity_class, '__init__'):
+			return
+
+		code = entity_class.__init__.__code__
+
+		# entity constructor hasn't any argument to pass
+		# just self
+		if code.co_argcount < 2: 
+			return
+
+		# return arguments without 'self'
+		init_args = code.co_varnames[1:]
+		init_annot = entity_class.__init__.__annotations__
+
+		for arg in init_args:
+			arg_name = '--' + entity + '-' + arg.replace('_','-')
+			arg_type = str
+			if arg in init_annot:
+				arg_type = init_annot[arg]
+
+			parser.add_argument(arg_name, type=arg_type)
+
+	def __instantiate_entity(self, entity, entity_class, args):
+		pass
+
+
+
 
 
 def start_cli_runner():
--- tests/test_runners.py
+++ tests/test_runners.py
@@ -1,4 +1,43 @@
 import unittest
 
+from FXT.runners import CliRunner
+
+class MockBrokerWithAnnotation:
+	def __init__(self, account_balance: int, margin: float):
+		pass
+
+class MockBrokerWithoutAnnotation:
+	def __init__(self, account_balance, margin):
+		pass
+
+class MockModel:
+	def __init__(self, instrument: list):
+		pass
+
+class MockLoader:
+	def load(self, classname):
+		classes = {
+			'FXT.brokers.BaseBrokerWithout' : MockBrokerWithoutAnnotation,
+			'FXT.brokers.BaseBroker': MockBrokerWithAnnotation,
+			'FXT.models.BaseModel': MockModel
+		}
+
+		return classes[classname]
+
 class TestCliRunner(unittest.TestCase):
-	pass
+
+	def test_cli(self):
+		options = [
+			'FXT.brokers.BaseBroker',
+			'FXT.models.BaseModel',
+			'--broker-account-balance', '2500',
+			'--broker-margin', '0.321',
+			'--model-instrument', 'USD,EUR'
+		]
+
+		r = CliRunner(MockLoader())
+		parsed = r.parse_arguments(options)
+
+		self.assertEqual(parsed.broker_account_balance, 2500)
+		self.assertEqual(parsed.broker_margin, 0.321)
+		self.assertEqual(parsed.model_instrument, ['USD','EUR'])
