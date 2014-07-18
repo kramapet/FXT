afab8439cafa24836556041a2037c5e3551721af
Petr Kramar <kramar@masquerade.cz>
2014-07-18 21:55:45 +1200

    Add support for list used as arguments

--- FXT/runners.py
+++ FXT/runners.py
@@ -17,8 +17,16 @@ class CliRunner(BaseRunner):
 	
 	def __init__(self, loader):
 		self.loader = loader
+		self.type_callbacks = {
+			list: self.__parse_type_list
+		}	
 
 	def parse_arguments(self, args):
+		"""Parse arguments
+
+		Keyword arguments:
+		args -- <list> arguments
+		"""
 		entities = ('broker', 'model')
 
 		broker_class = self.loader.load(args[0])
@@ -53,29 +61,44 @@ class CliRunner(BaseRunner):
 
 		code = entity_class.__init__.__code__
 
-		# entity constructor hasn't any argument to pass
+		# entity constructor has not any argument to pass
 		# just self
 		if code.co_argcount < 2: 
 			return
 
-		# return arguments without 'self'
+		# return argument names without 'self'
 		init_args = code.co_varnames[1:]
 		init_annot = entity_class.__init__.__annotations__
 
 		for arg in init_args:
+			# build long posix argument name
 			arg_name = '--' + entity + '-' + arg.replace('_','-')
+			# set default type to string
 			arg_type = str
+			
 			if arg in init_annot:
-				arg_type = init_annot[arg]
+				arg_type = self.__get_type_callback(init_annot[arg])
 
 			parser.add_argument(arg_name, type=arg_type)
 
 	def __instantiate_entity(self, entity, entity_class, args):
 		pass
 
+	def __parse_type_list(self, arg):
+		"""Parse argument type of list"""
+		return arg.split(',')
 
+	def __get_type_callback(self, arg_type):
+		"""Get function responsible for converting argument
+		to annotated argument
 
+		Keyword arguments:
+		arg_type -- argument type
+		"""
+		if arg_type in self.type_callbacks:
+			return self.type_callbacks[arg_type]
 
+		return arg_type
 
 def start_cli_runner():
 	"""used by setuptools. see setup.py
